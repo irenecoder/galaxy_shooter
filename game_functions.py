8094b462d5e6c4ef5462c49316cc634030bf9a55
irenecoder <wairene60@gmail.com>
2021-06-11 22:29:43 +0300

    Some refactoring done here

--- game_functions.py
+++ game_functions.py
@@ -7,19 +7,29 @@ def check_events(ship):
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type==pygame.KEYDOWN:
-                if event.key==pygame.K_RIGHT:
-                    ship.moving_right = True
-                elif event.key==pygame.K_LEFT:
-                    ship.moving_left= True
-
+                check_keydown(event,ship)
             elif event.type == pygame.KEYUP:
-                if event.key==pygame.K_RIGHT:
-                    ship.moving_right = False
-                    ship.rect.centerx +=1
-                elif event.key==pygame.K_LEFT:
-                    ship.moving_left = False
-                    #shift ship to the right
-                    ship.rect.centerx -=1
+                check_keyup(event,ship)
+
+                
+def check_keydown(event,ship):
+    #responds to keypresses of key
+    if event.key==pygame.K_RIGHT:
+
+        ship.moving_right = True
+    elif event.key==pygame.K_LEFT:
+        ship.moving_left= True
+
+            
+def check_keyup(event,ship):
+    #responds to key releases of key
+    if event.key==pygame.K_RIGHT:
+        ship.moving_right = False
+        ship.rect.centerx +=1
+    elif event.key==pygame.K_LEFT:
+        ship.moving_left = False
+        #shift ship to the right
+        ship.rect.centerx -=1
                 
 
             # elif event.type == pygame.KEYUP:
